a8b17a26175a05de1d74716351fba4523f59b69b
Allef Gomes <allefgalmeida@gmail.com>
2022-04-07 17:41:39 -0300

    add ex_machina, excoveralls and mix_test_watch as dependencie

--- phx_unit/mix.exs
+++ phx_unit/mix.exs
@@ -10,7 +10,11 @@ defmodule PhxUnit.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test
+      ]
     ]
   end
 
@@ -48,7 +52,10 @@ defmodule PhxUnit.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_machina, "~> 2.7.0"},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
